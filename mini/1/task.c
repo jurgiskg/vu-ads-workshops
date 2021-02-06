ebe99d82dc7c759ad0690435eff23a330ace6d4e
jurgiskg <jurgis.kargaudas@gmail.com>
2021-02-06 17:30:28 +0200

    update gitignore, prepare 1st mini assignment

---
+++ mini/1/task.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int sum(int a, int b) {
+    
+}
+
+? divAndMod(int a, int b) {
+
+}
+
+int main() {
+    int a = 42;
+    int b = 5;
+
+    int sumRes = sum(a, b);
+    printf("%d", sumRes);
+
+    ? divAndModRes = divAndMod(a, b);
+    // print div and mod
+    
+    return 0;
+}
