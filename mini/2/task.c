18e2662c53bb7ace64513fa900a2224a6c0bdc18
jurgiskg <jurgis.kargaudas@gmail.com>
2021-02-14 11:48:12 +0200

    add 2nd mini assignment

---
+++ mini/2/task.c
@@ -0,0 +1,22 @@
+#include <math.h>
+#include <stdio.h>
+
+typedef struct {
+    int x, y;
+} Point;
+
+? isRightTriangle(Point a, Point b, Point c) {
+
+}
+
+int main() {
+    //init points
+    Point a;
+    Point b;
+    Point c;
+
+    ? answer = isRightTriangle(a, b, c);
+    //print answer
+
+    return 0;
+}
